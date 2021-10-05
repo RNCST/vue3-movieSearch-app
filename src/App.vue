03243a98b995cb7a027f59b3b8c82e5055b93252
RNCST <rk51320928@gmail.com>
2021-10-05 12:06:16 +0900

    21/10/05 add footer

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <Logo />
   <Header />
   <RouterView />
+  <Footer />
   <!-- 
     main.js에 router를 사용하게되면 
     Vue-router가 제공하는 전역화되어있는 RouterView라는 컴포넌트를 사용해서 
@@ -11,11 +12,12 @@
 
 <script>
 import Header from '~/components/Header'
+import Footer from '~/components/Footer'
 
 export default {
   components:{
     Header,
-    
+    Footer
   }
 }
 </script>
