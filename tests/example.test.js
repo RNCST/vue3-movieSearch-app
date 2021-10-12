20a9c6794fcd358deebe55ffdb79404b7ab6bd6d
RNCST <rk51320928@gmail.com>
2021-10-12 17:55:22 +0900

    21/10/12 jest matchers example

--- tests/example.test.js
+++ tests/example.test.js
@@ -1,33 +1,23 @@
-import { double } from './example'
 
-describe('group1',() => {
-  beforeAll(() => {
-    // 모든 테스트가 실행되기전 한번 실행
-    console.log('beforeAll');
-  })
-  afterAll(() => { 
-    // 모든 테스트가 실행된 후 한번 실행
-    console.log('afterAll');
-  })
+const userA = {
+  name: 'osh',
+  age: 29
+}
 
-  beforeEach(() => {
-    // 각각 테스트가 실행되기전 실행
-    console.log('beforeEach');
-  })
-  afterEach(() => {
-    // 각각 테스트가 실행된후 실행
-    console.log('afterEach');
-  })
+const userB = {
+  name: 'sh o',
+  age: 28
+}
 
-  test('first test', () => {
-    expect(123).toBe(123)
+test('equal', () => {
+  expect(userA.age).toBe(29)
+  expect(userA).toEqual({
+    name:'osh',
+    age:29
   })
-  test('number param', () =>{
-    expect(double(3)).toBe(6)
-    expect(double(10)).toBe(20)
-  })
-  test('no param', () =>{
-    expect(double()).toBe(0)
-  })
-}) // test grouping
+})
 
+test('not equal', () => {
+  expect(userB.name).not.toBe('osh')
+  expect(userB).not.toBe(userA)
+})
